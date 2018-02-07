2678c00084d2acf8b240314a7fd069bedde40e36
Madeline Wong <madelinegwong@outlook.com>
2018-02-06 20:41:16 -0500

    added reset button!

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     }
     this.increment = this.increment.bind(this); //binding means inside of increment. the  "this" keyword is the component
     this.decrement = this.decrement.bind(this); 
+    this.reset = this.reset.bind(this);
   }
   increment(){
     this.setState({count: this.state.count + 1});
@@ -15,12 +16,16 @@ class App extends Component {
   decrement(){
   this.setState({count: this.state.count - 1});
 }
+ reset(){
+   this.setState({count: 0})
+ }
   render() {
     return (
       <div>
         <button onClick={this.increment}>+</button>
         <button onClick={this.decrement}>-</button>
        <h1>{this.state.count}</h1>
+       <button onClick={this.reset}>reset</button> 
       </div>
     );
   }
