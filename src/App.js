5c96b088a8ace26c33b06491ac169bd154051411
Madeline Wong <madelinegwong@outlook.com>
2018-02-07 19:55:55 -0500

    installed and applied bootstrap and classNames.

--- src/App.js
+++ src/App.js
@@ -4,28 +4,38 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      count: 0
+      count: 0, 
+      value: 1
     }
+  
     this.increment = this.increment.bind(this); //binding means inside of increment. the  "this" keyword is the component
     this.decrement = this.decrement.bind(this); 
     this.reset = this.reset.bind(this);
-  }
+    this.handleValueChange = this.handleValueChange.bind(this);
+}
   increment(){
-    this.setState({count: this.state.count + 1});
+    this.setState({count: this.state.count + this.state.value});
   }
   decrement(){
-  this.setState({count: this.state.count - 1});
+  this.setState({count: this.state.count + this.state.value});
 }
  reset(){
    this.setState({count: 0})
  }
+  handleValueChange(e){
+    this.setState({value: +e.target.value});
+  }
   render() {
     return (
       <div>
-        <button onClick={this.increment}>+</button>
-        <button onClick={this.decrement}>-</button>
+        <div className="form-group">
+        <input type="number" className="form-control" onChange={this.handleValueChange} value={this.state.value}/>
+        </div>
+        <button className="btn btn-primary" onClick={this.increment}>+</button>
+        <button className="btn btn-primary" onClick={this.decrement}>-</button>
        <h1>{this.state.count}</h1>
-       <button onClick={this.reset}>reset</button> 
+       <button className="btn btn-primary" onClick={this.reset}>reset</button> 
+       
       </div>
     );
   }
